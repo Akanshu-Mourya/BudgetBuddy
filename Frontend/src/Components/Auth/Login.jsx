92a74aded108bc06c3d91b8da75e1df24563f4b6
Akanshu-Mourya <akanshumourya9@gmail.com>
2025-01-27 12:40:18 +0530

    fix: update navigation from Login to Dashboard upon successful user authentication

--- Frontend/src/Components/Auth/Login.jsx
+++ Frontend/src/Components/Auth/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
     }
     useEffect(() => {
         if (user) {
-            navigate("/login");
+            navigate("/dashboard");
         }
     }, [user, navigate]);
     return (
