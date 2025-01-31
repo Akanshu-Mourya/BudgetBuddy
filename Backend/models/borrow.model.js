cb08f1c3d2d0571e2eb29d97c98308b5a03a2fc1
Akanshu Mourya <akanshus.bca2023@ssism.org>
2025-01-31 14:21:42 +0530

    remove unused DeshBord component from Dashboard

--- Backend/models/borrow.model.js
+++ Backend/models/borrow.model.js
@@ -1,55 +1,47 @@
 import mongoose from "mongoose";
 
 const borrowSchema = new mongoose.Schema({
-    borrower:
-    {
+    borrower: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User", required: true
-    }, // Borrower ID
-    lenderName:
-    {
+    },
+    // Borrower ID
+    lenderName: {
         type: String,
         required: true
-    }, // Name of the lender
-    amount:
-    {
+    },
+    // Name of the lender
+    amount: {
         type: Number,
         required: true
-    }, // Total borrowed amount
-    interestType:
-    {
+    },
+    // Total borrowed amount
+    interestType: {
         type: String,
         enum: ["simple", "compound"],
         required: true
     },
-    interestRate:
-    {
+    interestRate: {
         type: Number,
         required: true
     },
     // Interest rate in percentage
-    borrowDate:
-    {
+    borrowDate: {
         type: Date,
         default: Date.now
     },
-    dueDate:
-    {
+    dueDate: {
         type: Date,
         required: true
     },
-    paidAmount:
-    {
+    paidAmount: {
         type: Number,
         default: 0
     }, // Amount repaid by borrower
-    status:
-    {
+    status: {
         type: String,
         enum: ["borrowed", "paid"],
         default: "borrowed"
     },
 });
-
-
 export const Borrow = mongoose.model('Borrow', borrowSchema);
